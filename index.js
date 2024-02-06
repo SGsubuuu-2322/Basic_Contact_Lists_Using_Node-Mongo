11578cec5c555a69ebbf6eb9f9f7dc50a47628ec
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-02-06 15:41:06 +0530

    Created a index.html file and then serve it to the response end...

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require("http");
+const fs = require("fs");
 
 const port = 8000;
 
 function requestHandler(req, res) {
-  console.log(req.url);
+  //   console.log(req.url);
 
   res.writeHead(200, { "content-type": "text/html" });
 
-  res.end("<h1>Gotchaa!!!</h1>");
+  //   res.end("<h1>Gotchaa!!!</h1>");
+
+  fs.readFile("./index.html", function (err, data) {
+    if (err) {
+      console.log("Error: " + err);
+      return res.end("<h1>Error!!!</h1>");
+    }
+
+    return res.end(data);
+  });
 }
 
 const server = http.createServer(requestHandler);
